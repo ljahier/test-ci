a31330223225d689f40deba1b625a549a10126b6
Lucas JAHIER <lucas@jahier.dev>
2020-06-29 03:47:36 +0200

    add new routes and unit test linked

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require('express');
 const { json } = require('express');
 const app = express();
 
+let users = [
+    {
+        "id": 0,
+        "username": "john",
+        "name": "John Doe",
+        "email": "john@doe.com"
+    },
+    {
+        "id": 1,
+        "username": "jean",
+        "name": "Jean Dupond",
+        "email": "jean@dupond.com"
+    },
+    {
+        "id": 2,
+        "username": "mike",
+        "name": "Mike Parker",
+        "email": "mike@parker.com"
+    }
+]
+
 app.get('/', (req, res) => {
     res.json({"result": "Hello World"});
 })
 
 app.get('/hello/:name', (req, res) => {
     res.json({"result": `Hello ${req.params.name}`})
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
 app.listen(8080);
--- test/test.js
+++ test/test.js
@@ -10,3 +10,13 @@ describe('GET /hello/lucas', function () {
             .expect(200, done);
     });
 });
+
+describe('GET /users', function () {
+    it('responds with json', function (done) {
+        request(app)
+            .get('/users')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
